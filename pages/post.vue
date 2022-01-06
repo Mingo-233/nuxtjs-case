f2ce4e31d2af1029e52e4aebdc70014551b71ec2
王佳明 <mingo@wangjiangdeiMac.lan>
2022-01-06 14:06:05 +0800

    feat:layouts

---
+++ pages/post.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- Your template -->
+  <div>this is post vue</div>
+</template>
+<script>
+  export default {
+    layout: 'blog'
+    // page component definitions
+  }
+</script>
