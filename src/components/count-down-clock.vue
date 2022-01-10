20dda322c282cd3af112f9a1bc79a393ac5a713e
mingo <mingo233@qq.com>
2022-01-10 11:40:53 +0800

    feat: init src dir

---
+++ src/components/count-down-clock.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      time 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'count-down-clock',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+
+  },
+  mounted () {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/app/okk.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <p>app  . index vue</p>
+    <count-down-clock></count-down-clock>
+  </div>
+</template>
+
+<script>
+import countDownClock from '@/components/count-down-clock'
+// import CountDownClock from '../../components/count-down-clock.vue';
+export default {
+  components:{
+    countDownClock
+    // CountDownClock
+  }
+};
+</script>
