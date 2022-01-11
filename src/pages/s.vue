67b337985a938c474a6d1b2fbcfc1b2b04e81e31
mingo <mingo233@qq.com>
2022-01-11 15:56:07 +0800

    feat: sup ts

--- src/pages/s.vue
+++ src/pages/s.vue
@@ -1,26 +1,94 @@
 <template>
   <div>
- sss .vue
+    sss .vue
+    {{ msg }}
   </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
-  name: '',
-  data () {
+  name: "ts",
+  data() {
     return {
-      msg: ''
-    }
+      msg: "",
+    };
   },
-  methods: {
-
-  },
-  mounted () {
-
-  }
+  methods: {},
+  mounted() {
+    (this as any).msg = "dd";
+    type TypeA = '123'
+type TypeB ={
+  a: string
 }
+type TypeC = {
+  c: number
+}
+type TypeD =TypeB&TypeC
+let obj:TypeB={
+  a:'22'
+}
+let nn:TypeA = '123'
+// console.log(nn);
+// console.log(obj);
+
+let qq:TypeD = {
+  a:'pp',
+  c:22
+}
+  },
+};
+
+interface People {
+  a: string;
+  b: string;
+}
+
+// 返回 'a' | 'b'
+type KeyofPeople = keyof People;
+let d:KeyofPeople = 'b'
+// type KeyofPeople = 'a' | 'b';
+
+const obj = { a: '123', b: 123 }
+type Obj = typeof obj;
+let pp:Obj={
+  a:'sss',
+  b:0
+}
+
+enum E1 {
+  A,
+  B,
+  C
+}
+
+type TE1 = keyof E1;
+/**
+拿到的是错误的类型
+type TE1 = "toString" | "toFixed" | "toExponential" | "toPrecision" | "valueOf" | "toLocaleString"
+*/
+
+type TE2 = keyof typeof E1;
+let te2:TE2 = 'A'
+/**
+拿到的是正确的类型
+type TE2 = "A" | "B" | "C"
+*/
+
+interface Obj1<T> {
+  a: T
+}
+let o1:Obj1<number> = {
+  a:312
+}
+type Fn2 = <T extends number>(...args: any[]) => any;
+// 函数泛型
+type Fn1 = <T>(...args: any[]) => any;
+
+const ff:Fn2 =function(a,b){
+  console.log(a);
+  
+}
+ff('ddd')
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
