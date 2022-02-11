0c7737a0948cdbf47e0ad0adafff750c20662697
mingo <mingo233@qq.com>
2022-02-11 11:25:06 +0800

    feat: jsdom

--- src/pages/promise.vue
+++ src/pages/promise.vue
@@ -1,80 +1,94 @@
 <template>
   <div class="css-demo">
-
-    <el-input v-model="a"
-              @blur="blurHandle" />
+    <el-input v-model="a" @blur="blurHandle" />
     <button @click="submit">TIJIAO</button>
 
-    <hr>
-    <el-input v-model="b"
-              @blur="blurHandle2" />
+    <hr />
+    <el-input v-model="b" @blur="blurHandle2" />
     <button @click="submit2">TIJIAO</button>
   </div>
 </template>
 
 <script>
-
+// const { loadNuxt } = require("nuxt");
 export default {
-  name: 'promise-demo',
-  data () {
+  name: "promise-demo",
+  data() {
     return {
-      a: '',
+      a: "",
       ajaxLoading: null,
-      b: '',
-      ajaxLoading2: Promise.resolve()
-    }
+      b: "",
+      ajaxLoading2: Promise.resolve(),
+    };
   },
   methods: {
-    submit () {
+    submit() {
       if (this.ajaxLoading) {
-        this.ajaxLoading.then(res => {
-          console.log('submit');
-        })
+        this.ajaxLoading.then((res) => {
+          console.log("submit");
+        });
       } else {
-        console.log('submit');
+        console.log("submit");
       }
+      window.location.href='/'
     },
-    blurHandle () {
-      console.log('ajax start');
+    blurHandle() {
+      console.log("ajax start");
       this.ajaxLoading = new Promise((resolve) => {
         console.log(this);
-        this.ajaxRequest().then(res => {
-          resolve()
-          console.log('ajax end');
-        }).finally(() => {
-          this.ajaxLoading = null
-        })
-      })
+        this.ajaxRequest()
+          .then((res) => {
+            resolve();
+            console.log("ajax end");
+          })
+          .finally(() => {
+            this.ajaxLoading = null;
+          });
+      });
     },
-    submit2 () {
+    submit2() {
       console.log(this.ajaxLoading2);
-      this.ajaxLoading2.then(res => {
-        console.log('submit');
-      })
-
+      this.ajaxLoading2.then((res) => {
+        console.log("submit");
+      });
     },
-    blurHandle2 () {
-      console.log('ajax start');
+    blurHandle2() {
+      console.log("ajax start");
       this.ajaxLoading2 = new Promise((resolve) => {
         console.log(this);
-        this.ajaxRequest().then(res => {
-          resolve()
-          console.log('ajax end');
-        }).finally(() => {
-          this.ajaxLoading2 = Promise.resolve()
-        })
-      })
+        this.ajaxRequest()
+          .then((res) => {
+            resolve();
+            console.log("ajax end");
+          })
+          .finally(() => {
+            this.ajaxLoading2 = Promise.resolve();
+          });
+      });
     },
-    ajaxRequest () {
+    ajaxRequest() {
       return new Promise((resolve) => {
         setTimeout(() => {
-          resolve('ok')
+          resolve("ok");
         }, 2000);
-      })
+      });
+    },
+  },
+  created() {
+    async function init() {
+      // Assuming you've already built your project
+      // const nuxt = await loadNuxt({ for: "start" });
+      // console.log(nuxt);
+      // await nuxt.listen(3000);
+      // const window = await nuxt.renderAndGetWindow("http://localhost:3000");
+      // // Display the head `<title>`
+      // console.log(window.document.title);
+      // nuxt.close();
     }
+console.log();
+    init();
   },
 };
 </script>
 
-<style lang="less" scoped>
-</style>
+<style lang="less" scoped></style>
