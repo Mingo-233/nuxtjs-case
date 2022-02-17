b49a3f0edbd0b415cfc38ad8f497541cbcd8b316
mingo <mingo233@qq.com>
2022-02-17 13:49:41 +0800

    feat: ant

---
+++ src/pages/a.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <!-- <div class="box"> -->
+      <div class="top">
+        <a-button type="primary"> Primary </a-button>
+        <a-popover title="Title" :getPopupContainer="getPopupContainer" trigger="click">
+          <template slot="content">
+            <p>Content</p>
+            <p>Content</p>
+          </template>
+          <a-button type="primary"> Hover me </a-button>
+        </a-popover>
+      </div>
+    <!-- </div> -->
+
+    <div class="section">
+      <ul>
+        <li>a</li>
+        <li>a</li>
+        <li>a</li>
+        <li>a</li>
+        <li>a</li>
+        <li>a</li>
+        <li>a</li>
+        <li>a</li>
+        <li>a</li>
+        <li>a</li>
+        <li>a</li>
+        <li>a</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "a",
+  methods: {
+      getPopupContainer(){
+          return document.querySelector('.top')
+      }
+  },
+};
+</script>
+<style lang="less" scoped>
+// .box {
+//   height: 100px;
+
+//   .top {
+//     position: fixed;
+//     display: flex;
+//     align-items: center;
+//     width: 100%;
+//     height: 100px;
+//     background: brown;
+//   }
+// }
+  .top {
+    position: fixed;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 100px;
+    background: brown;
+  }
+.section {
+  width: 100%;
+  height: 2500px;
+  background: burlywood;
+}
+</style>
