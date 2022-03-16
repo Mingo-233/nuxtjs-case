21f227afa8ceab6e82c1fc455615fb40719ccc0f
mingo <mingo233@qq.com>
2022-03-16 11:13:18 +0800

    feat: _id

--- src/pages/user/_id.vue
+++ src/pages/user/_id.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="container">
     <p>_ id.vue</p>
+    <p>{{aid}}</p>
   </div>
 </template>
 
 <script>
 export default {
-  // validate({ params }) {
-  //   console.log(params);
-  //   console.log(params.id);
-  //   // 必须是number类型
-  //   return /^\d+$/.test(params.id);
-  //   // return true
-  // },
+  validate({ params }) {
+    // console.log(params);
+    // console.log(params.id);
+    // this.aid = params.id
+// console.log(this.aid);
+
+    // 必须是number类型
+    return /^\d+$/.test(params.id);
+    // return true
+  },
   async asyncData({ params }) {
+    console.log('asyncData');
     console.log(params);
+    console.log(typeof params);
+    console.log(params.id);
+
+    // this.id = params.id
     // const slug = params.slug; // When calling /abc the slug will be "abc"
     // return { slug };
+  },
+  data(){
+    return{
+      aid:'3'
+    }
   },
   mounted(){
 
---
+++ src/pages/user/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <p>_ id.vue index</p>
+  </div>
+</template>
+
+<script>
+export default {
+  // validate({ params }) {
+  //   console.log(params);
+  //   console.log(params.id);
+  //   // 必须是number类型
+  //   return /^\d+$/.test(params.id);
+  //   // return true
+  // },
+  async asyncData({ params }) {
+    console.log(params);
+    // const slug = params.slug; // When calling /abc the slug will be "abc"
+    // return { slug };
+  },
+  mounted(){
+
+  }
+};
+</script>
