17b2c824b7128e53b612a71959c0482fde9be059
mingo <mingo233@qq.com>
2022-03-21 21:22:41 +0800

    feat: cssdemo

--- src/pages/css.vue
+++ src/pages/css.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="css-demo">
-    <div class="banner">banner</div>
+    <div class="banner" :style="{background:`url(//yun.baoxiaohe.com/static/temp_show_img2-1.png?imageView2/0/w/385/h/166/q/90|imageslim)`}">banner</div>
     <div class="content">
       aa
     </div>
@@ -24,6 +24,8 @@ export default {
   .banner {
     position: fixed;
     width: 100%;
+    height: 100px;
+    background-image: url("//yun.baoxiaohe.com/static/temp_show_img2-1.png?imageView2/0/w/385/h/166/q/90|imageslim");
     background-color: antiquewhite;
   }
   .content {
