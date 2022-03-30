bd68d53f0e70f3286ad1673b317a35b19d48f9c3
mingo <mingo233@qq.com>
2022-03-30 10:07:28 +0800

    feat: alet

--- src/pages/_slug/comments.vue
+++ src/pages/_slug/comments.vue
@@ -7,11 +7,19 @@
 <script>
 export default {
   async asyncData(content) {
-    console.log(content);
+    // console.log(content);
     content.$myInjectedFunction("works in comments asyncData");
   },
   mounted(){
+    alert("mounted");
 
-  }
+  },
+  beforeDestroy(){
+    alert("beforeDestroy");
+    console.log('beforeDestroy');
+  },
+  destroyed(){
+    console.log('destroyed');
+  },
 };
 </script>
