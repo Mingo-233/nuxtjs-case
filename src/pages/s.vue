296e715e7ea3bf251411b8a6616df08a9ae0cf9c
mingo <mingo233@qq.com>
2022-05-16 17:24:29 +0800

    feat: carousel

--- src/pages/s.vue
+++ src/pages/s.vue
@@ -8,16 +8,40 @@
       <Button>Click to Upload </Button>
     </Upload>
     <Button type="danger">Danger</Button>
+    <a-carousel arrows>
+      <div
+        slot="prevArrow"
+        slot-scope="props"
+        class="custom-slick-arrow"
+        style="left: 10px; z-index: 2"
+      >
+        <a-icon type="left-circle" />
+      </div>
+      <div
+        slot="nextArrow"
+        slot-scope="props"
+        class="custom-slick-arrow"
+        style="right: 10px"
+      >
+        <a-icon type="right-circle" />
+      </div>
+      <div><h3>1</h3></div>
+      <div><h3>2</h3></div>
+      <div><h3>3</h3></div>
+      <div><h3>4</h3></div>
+    </a-carousel>
   </div>
 </template>
 
 <script lang="ts">
-import { Button, Upload } from "ant-design-vue";
+import { Button, Upload, Icon, Carousel } from "ant-design-vue";
 
 export default {
   components: {
     Button,
     Upload,
+    aIcon: Icon,
+    aCarousel: Carousel,
   },
   methods: {
     handle() {},
@@ -33,4 +57,31 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.ant-carousel /deep/ .slick-slide {
+  text-align: center;
+  height: 160px;
+  line-height: 160px;
+  background: #364d79;
+  overflow: hidden;
+}
+
+.ant-carousel /deep/ .custom-slick-arrow {
+  width: 25px;
+  height: 25px;
+  font-size: 25px;
+  color: #fff;
+  background-color: rgba(31, 45, 61, 0.11);
+  opacity: 0.3;
+}
+.ant-carousel /deep/ .custom-slick-arrow:before {
+  display: none;
+}
+.ant-carousel /deep/ .custom-slick-arrow:hover {
+  opacity: 0.5;
+}
+
+.ant-carousel /deep/ .slick-slide h3 {
+  color: #fff;
+}
+</style>
